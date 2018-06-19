ceecd19cbb1cfad602aa382a2723c31e541e1d8d
Simon <simon@gehring.tv>
2018-06-19 20:28:49 +0200

    Added first exceptions to ExampleFactory

--- src/main/java/gehring/simon/hobby/swagger/testing/ExampleFactory.java
+++ src/main/java/gehring/simon/hobby/swagger/testing/ExampleFactory.java
@@ -5,32 +5,52 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import gehring.simon.hobby.swagger.model.v3_0.Example;
 import gehring.simon.hobby.swagger.model.v3_0.Parameter;
+import gehring.simon.hobby.swagger.model.v3_0.Schema;
 
 public class ExampleFactory {
 
-    public static String buildCustomExample(final Parameter para) {
-        // TODO: Implement this
-        return null;
-    }
+	public static String buildCustomExampleBySchema(Schema schema) {
+		return null;
+	}
 
-    // TODO: Evtl. mehrere examples benutzen?
-    public static String getExampleObject(final Parameter parameter) {
-        if (parameter.getExample() == null) {
-            if (parameter.getExample() == null || parameter.getExamples().isEmpty())
-                return buildCustomExample(parameter);
-            return buildExampleFromExample(parameter.getExamples().values().iterator().next());
-        }
-        return "";
-    }
+	public static String buildCustomExample(final Parameter para) {
+		/*
+		 * Either schema or content is set. Not both.
+		 */
+		if (para.getSchema() == null && para.getContent() == null)
+			throw new MalformedSwaggerYamlException("Parameter '" + para.getName() + "' with description '"
+					+ para.getDescription() + "' has neither a schema nor a content definition.");
 
-    public static String buildExampleFromExample(final Example paraExample) {
-        // TODO: global variable?
-        ObjectMapper m = new ObjectMapper();
-        try {
-            return m.writeValueAsString(paraExample);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-    }
+		if (para.getContent() != null && para.getSchema() != null)
+			throw new MalformedSwaggerYamlException("Parameter '" + para.getName() + "' with description '"
+					+ para.getDescription() + "' has a schema and a content definition. Cannot have both.");
+
+		if (para.getSchema() != null) {
+			return buildCustomExampleBySchema(para.getSchema());
+		} else {
+			// para.getContent != null
+		}
+		return null;
+	}
+
+	// TODO: Evtl. mehrere examples benutzen?
+	public static String getExampleObject(final Parameter parameter) {
+		if (parameter.getExample() == null) {
+			if (parameter.getExample() == null || parameter.getExamples().isEmpty())
+				return buildCustomExample(parameter);
+			return buildExampleFromExample(parameter.getExamples().values().iterator().next());
+		}
+		return "";
+	}
+
+	public static String buildExampleFromExample(final Example paraExample) {
+		// TODO: global variable?
+		ObjectMapper m = new ObjectMapper();
+		try {
+			return m.writeValueAsString(paraExample);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 }
--- src/main/java/gehring/simon/hobby/swagger/testing/HttpHandler.java
+++ src/main/java/gehring/simon/hobby/swagger/testing/HttpHandler.java
@@ -13,40 +13,48 @@ import gehring.simon.hobby.swagger.testing.http.Method;
 
 public class HttpHandler {
 
-    public static String encodeQueryParameter(final String key, final String value) {
-        try {
-            return URLEncoder.encode(key, "UTF-8") + "=" + URLEncoder.encode(value, "UTF-8");
-        } catch (UnsupportedEncodingException e1) {
-            throw new RuntimeException("Your computer doesn't seem to understand UTF-8. Throw it away!", e1);
-        }
-    }
+	public static String encodeQueryParameter(final String key, final String value) {
+		if (key == null && value == null)
+			throw new IllegalArgumentException("Trying to encode query parameter, but key and value are null.");
+		if (key == null)
+			throw new IllegalArgumentException(
+					"Trying to encode query parameter, but key is null. Value is '" + value + "'.");
+		if (value == null)
+			throw new IllegalArgumentException(
+					"Trying to encode query parameter, but value is null. Key is '" + key + "'.");
+		try {
+			return URLEncoder.encode(key, "UTF-8") + "=" + URLEncoder.encode(value, "UTF-8");
+		} catch (UnsupportedEncodingException e1) {
+			throw new RuntimeException("Your computer doesn't seem to understand UTF-8. Throw it away!", e1);
+		}
+	}
 
-    public static URL getFinalServerUrl(final Server target, final String path) {
-        URL serverUrl;
-        try {
-            serverUrl = new URL(target.getUrl() + path);
-        } catch (MalformedURLException e) {
-            throw new MalformedSwaggerYamlException("Server URL '" + target.getUrl() + path + "' malformed.", e);
-        }
-        return serverUrl;
-    }
+	public static URL getFinalServerUrl(final Server target, final String path) {
+		URL serverUrl;
+		try {
+			serverUrl = new URL(target.getUrl() + path);
+		} catch (MalformedURLException e) {
+			throw new MalformedSwaggerYamlException("Server URL '" + target.getUrl() + path + "' malformed.", e);
+		}
+		return serverUrl;
+	}
 
-    public static HttpURLConnection getHttpConnection(final URL serverUrl, final Method how) {
-        HttpURLConnection con;
-        try {
-            con = (HttpURLConnection) serverUrl.openConnection();
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to connect to server " + serverUrl + ".", e);
-        }
-        try {
-            con.setRequestMethod(how.name());
-        } catch (ProtocolException e) {
-            throw new MalformedSwaggerYamlException(
-                    "An unknown method has been requested: '" + how.name() + "' (" + how.toString() + ").", e);
-        }
-        // TODO: Hardcoded?
-        con.setRequestProperty("accept", "application/json");
-        con.setRequestProperty("content-type", "application/json");
-        return con;
-    }
+	public static HttpURLConnection getHttpConnection(final URL serverUrl, final Method how) {
+		HttpURLConnection con;
+		try {
+			con = (HttpURLConnection) serverUrl.openConnection();
+		} catch (IOException e) {
+			throw new RuntimeException("Failed to connect to server " + serverUrl + ".", e);
+		}
+		try {
+			con.setRequestMethod(how.name());
+		} catch (ProtocolException e) {
+			throw new MalformedSwaggerYamlException(
+					"An unknown method has been requested: '" + how.name() + "' (" + how.toString() + ").", e);
+		}
+		// TODO: Hardcoded?
+		con.setRequestProperty("accept", "application/json");
+		con.setRequestProperty("content-type", "application/json");
+		return con;
+	}
 }
