d89520a223602484e2409cb10b4518d4dad64840
Simon <simon@gehring.tv>
2018-06-25 20:03:32 +0200

    Need the concept of a hashmap, but don't want to call it that way.

---
+++ src/main/java/gehring/simon/hobby/swagger/testing/JsonObject.java
@@ -0,0 +1,27 @@
+package gehring.simon.hobby.swagger.testing;
+
+import java.util.HashMap;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+class JsonObject extends HashMap<String, Object> {
+
+	@Override
+	public String toString() {
+		try {
+			final ObjectMapper x = new ObjectMapper();
+			x.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
+			x.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+			x.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false);
+
+			return x.writeValueAsString(this);
+		} catch (final JsonProcessingException e) {
+			throw new RuntimeException("Failed to serialize object to JSON.", e);
+		}
+	}
+
+}
