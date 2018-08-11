3b6e502ed3cb2074ccf223ec82f8eedb63cb2551
Simon Gehring <simon@gehring.tv>
2018-08-12 01:05:05 +0200

    Serial Version ID

--- src/main/java/gehring/simon/hobby/swagger/testing/NotImplementedException.java
+++ src/main/java/gehring/simon/hobby/swagger/testing/NotImplementedException.java
@@ -2,4 +2,9 @@ package gehring.simon.hobby.swagger.testing;
 
 public class NotImplementedException extends IllegalStateException {
 
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1897590956009745840L;
+
 }
